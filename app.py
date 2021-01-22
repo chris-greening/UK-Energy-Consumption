dc1afc6a7c1cebccc376c76fda76623034fab331
chris-greening <chrisgreening3@gmail.com>
2021-01-22 18:17:44 -0500

    add: added line plot and stylings

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
+from plotly.graph_objs import *
 import pandas as pd
 
-from data_processing import preprocess_dataframe, melt_total_dataframe
+from data_processing import preprocess_dataframe, melt_dataframe
 
 def generate_table(dataframe, max_rows=10):
     return html.Table([
@@ -35,24 +36,42 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div([
-    html.Div([
-        dcc.Graph(
-                id='total-energy-consumption-bar',
-                hoverData={'points': [{'customdata': 'South East'}]}
-            ),
-        dcc.Slider(
-                id='total-energy-consumption-year-slider',
-                min=dff['Year'].min(),
-                max=dff['Year'].max(),
-                value=dff['Year'].max(),
-                marks={str(year): str(year) for year in dff['Year'].unique()},
-                step=None
-            )
-        ],
-        id="bar-plot"),
+app.layout = html.Div(
+    children = [
+        html.Div(
+            children = [
+                dcc.Graph(
+                        id='total-energy-consumption-bar',
+                    hoverData={'points': [{'curveNumber': 2, 'pointNumber': 2, 'pointIndex': 2,
+                                           'x': 'Greater London', 'y': 36021.306273, 'label': 'Greater London', 'value': 36021.306273}]},
+                        style={'height': '60vh'}
+                    ),
+                dcc.Slider(
+                        id='total-energy-consumption-year-slider',
+                        min=dff['Year'].min(),
+                        max=dff['Year'].max(),
+                        value=dff['Year'].max(),
+                        marks={str(year): str(year) for year in dff['Year'].unique()},
+                        step=None
+                    )
+            ],
+            className="top-plot"
+        ),
+        html.Div(
+            children = [
+                dcc.Graph(
+                    id="region-time-series-bar",
+                    style={'height': '30vh'}
+                ),
+                dcc.Graph(
+                    id="region-time-series-scatter",
+                    style={'height': '30vh'}
+                ),
+            ],
+            className="top-plot"
+        ),
     ],
-    className="dash"
+    id="dash"
 )
 
 
@@ -62,10 +81,11 @@ app.layout = html.Div([
 )
 def update_graph(year_value):
     min_y = 0
-    max_y = int(dff.groupby(["Year", "Name"]).sum().max())
+    max_y = int(dff.groupby(["Year", "Name"]).sum().max()['All_Fuels_Total'])
+    max_y = max_y + max_y*.05
 
     year_df = dff[dff["Year"] == year_value]
-    long_year_df = melt_total_dataframe(year_df)
+    long_year_df = melt_dataframe(year_df)
     fig = px.bar(
         long_year_df,
         x="Name",
@@ -74,6 +94,56 @@ def update_graph(year_value):
         color_discrete_map=energy_source_colors,
         range_y=[min_y, max_y]
     )
+    fig.update_layout({
+        'plot_bgcolor': '#F2F8FF',
+        'paper_bgcolor': '#F2F8FF'
+    })
+    return fig
+
+@app.callback(
+    Output('region-time-series-bar', 'figure'),
+    Input('total-energy-consumption-bar', 'hoverData')
+)
+def update_region_time_series(hoverData):
+    region_df = dff[dff['Name'] == hoverData['points'][0]['x']]
+    long_region_df = melt_dataframe(region_df)
+
+    title = f"Energy consumption time series for {hoverData['points'][0]['x']}"
+    fig = px.bar(
+            long_region_df,
+            x="Year",
+            y="GWh",
+            color="Energy type",
+            title=title,
+            color_discrete_map=energy_source_colors,
+        )
+    fig.update_layout({
+        'plot_bgcolor': '#F2F8FF',
+        'paper_bgcolor': '#F2F8FF'
+    })
+    return fig
+
+
+@app.callback(
+    Output('region-time-series-scatter', 'figure'),
+    Input('total-energy-consumption-bar', 'hoverData')
+)
+def update_region_time_series(hoverData):
+    region_df = dff[dff['Name'] == hoverData['points'][0]['x']]
+    long_region_df = melt_dataframe(region_df)
+
+    title = f"Energy consumption time series for {hoverData['points'][0]['x']}"
+    fig = px.line(
+        long_region_df,
+        x="Year",
+        y="GWh",
+        color="Energy type",
+        color_discrete_map=energy_source_colors,
+    )
+    fig.update_layout({
+        'plot_bgcolor': '#F2F8FF',
+        'paper_bgcolor': '#F2F8FF'
+    })
     return fig
 
 if __name__ == '__main__':
