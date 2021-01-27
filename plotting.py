0d37353b89305303068befac88d38a56715f9b11
chris-greening <chrisgreening3@gmail.com>
2021-01-27 13:20:50 -0500

    refactor: refactored some of the plotting data

---
+++ plotting.py
@@ -0,0 +1,12 @@
+ENERGY_SOURCE_COLORS = {
+    "Coal": '#525B76',
+    "Manufactured": '#F4A259',
+    "Petroleum": '#7A89C2',
+    "Gas": '#F9B5AC',
+    "Electricity": '#EE7674',
+    "Bioenergy": '#9DBF9E',
+}
+PLOT_COLORS = {
+    'plot_bgcolor': '#F2F8FF',
+    'paper_bgcolor': '#F2F8FF'
+}
