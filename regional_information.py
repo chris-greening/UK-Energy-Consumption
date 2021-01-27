c1501da36544c30acae6468bd8a6cea58ecdf4e3
chris-greening <chrisgreening3@gmail.com>
2021-01-27 13:21:05 -0500

    add: added dict of regional information for constructing markdown

---
+++ regional_information.py
@@ -0,0 +1,50 @@
+regional_information = {
+    "South East": {
+        "description": "South East England is the third largest region of England, with an area of 19,096 sq km (7,373 sq mi), and is also the most populous with a total population of over eight and a half million (2011). The region contains seven legally chartered cities: Brighton and Hove, Canterbury, Chichester, Oxford, Portsmouth, Southampton and Winchester. The region's close proximity to London and connections to several national motorways have led to South East England becoming a prosperous economic hub with the largest economy of any region in the UK, after London. The region is home to Gatwick Airport, the UK's second-busiest airport, and Heathrow Airport (the UK's busiest airport) is located adjacent to the region's boundary with Greater London. The coastline along the English Channel provides numerous ferry crossings to mainland Europe.",
+        "source": "https://en.wikipedia.org/wiki/South_East_England"
+    },
+    "East Midlands": {
+        "description": "The East Midlands is one of nine official regions of England at the first level of NUTS for statistical purposes. It consists of Derbyshire, Leicestershire, Lincolnshire (except North and North East Lincolnshire), Northamptonshire, Nottinghamshire and Rutland. The region has an area of 15,627 sq km (6,034 sq mi), with a population over 4.5 million in 2011. The most populous settlements in the region are Derby, Leicester, Lincoln, Mansfield, Northampton and Nottingham. Other notable settlements include Boston, Chesterfield, Corby, Grantham, Hinckley, Kettering, Loughborough, Newark-on-Trent, Skegness, Wellingborough, and Worksop.",
+        "source": "https://en.wikipedia.org/wiki/East_Midlands"
+    },
+    "East Of England": {
+        "description": "The East of England region was created in 1994 and adopted for statistics purposes from 1999. It includes the ceremonial counties of Bedfordshire, Cambridgeshire, Essex, Hertfordshire, Norfolk and Suffolk. Essex has the highest population in the region. The population of the East of England region at the 2011 census was 5,847,000. Bedford, Luton, Basildon, Peterborough, Southend-on-Sea, Norwich, Ipswich, Colchester, Chelmsford and Cambridge are the region's most populous towns. The southern part of the region lies in the London commuter belt.",
+        "source": "https://en.wikipedia.org/wiki/East_of_England"
+    },
+    "London": {
+        "description": "Greater London is a ceremonial county of England that makes up the majority of the London region. This region forms the administrative boundaries of London and is organised into 33 local government districts—the 32 London boroughs and the City of London, which is located within the region but is separate from the county. The region covers 1,572 sq km (607 sq mi) and had a population of 8,174,000 at the 2011 census.",
+        "source": "https://en.wikipedia.org/wiki/Greater_London"
+    },
+    "North East": {
+        "description": "North East England is one of nine official regions of England at the first level of NUTS for statistical purposes. The region includes the counties of Tyne and Wear, County Durham, Northumberland and a small part of North Yorkshire. Large settlements include the cities of Newcastle upon Tyne, Sunderland and the City of Durham and towns of Gateshead, Darlington, Middlesbrough, Stockton-on-Tees and Hartlepool.",
+        "source": "https://en.wikipedia.org/wiki/North_East_England"
+    },
+    "North West": {
+        "description": "North West England is one of nine official regions of England and consists of the counties of Cheshire, Cumbria, Greater Manchester, Lancashire and Merseyside. The North West had a population of 7,052,000 in 2011. It is the third-most-populated region in the United Kingdom, after the South East and Greater London. The largest settlements are Manchester, Liverpool, Warrington, Blackpool, Preston, Stockport, Sale and Bolton.",
+        "source": "https://en.wikipedia.org/wiki/North_West_England"
+    },
+    "Northern Ireland": {
+        "description": "Northern Ireland (Irish: Tuaisceart Éireann; Ulster-Scots: Norlin Airlann) is variously described as a country, province, or region which is part of the United Kingdom. Located in the northeast of the island of Ireland, Northern Ireland shares a border to the south and west with the Republic of Ireland. In 2011, its population was 1,810,863, constituting about 30% of the island's population and about 3% of the UK's population.",
+        "source": "https://en.wikipedia.org/wiki/Northern_Ireland"
+    },
+    "Scotland": {
+        "description": "Scotland (Scots: Scotland, Scottish Gaelic: Alba) is a country that is part of the United Kingdom. Covering the northern third of the island of Great Britain, mainland Scotland has a 96-mile (154 km) border with England to the southeast and is otherwise surrounded by the Atlantic Ocean to the north and west, the North Sea to the northeast and the Irish Sea to the south. Scotland is the second largest country in the United Kingdom, and accounted for 8.3% of the population in 2012.",
+        "source": "https://en.wikipedia.org/wiki/Scotland"
+    },
+    "South West": {
+        "description": "South West England is one of nine official regions of England. It consists of the counties of Bristol, Cornwall (including the Isles of Scilly), Dorset, Devon, Gloucestershire, Somerset and Wiltshire. Large cities and towns in the region include Bristol, Cheltenham, Exeter, Gloucester, Plymouth and Swindon. It is geographically the largest of the nine regions of England covering 23,800 sq km (9,200 sq miles), but the third least-populous, with approximately five million residents.",
+        "source": "https://en.wikipedia.org/wiki/South_West_England"
+    },
+    "Wales": {
+        "description": "Wales (Welsh: Cymru) is a country that is part of the United Kingdom. It is bordered by England to the east, the Irish Sea to the north and west, and the Bristol Channel to the south. It had a population in 2011 of 3,063,456 and has a total area of 20,779 sq km (8,023 sq mi). Wales has over 1,680 miles (2,700 km) of coastline and is largely mountainous with its higher peaks in the north and central areas, including Snowdon (Yr Wyddfa), its highest summit.",
+        "source": "https://en.wikipedia.org/wiki/Wales"
+    },
+    "West Midlands": {
+        "description": "The West Midlands is one of nine official regions of England at the first level of NUTS for statistical purposes. It covers the western half of the area traditionally known as the Midlands. The region consists of the counties of Herefordshire, Shropshire, Staffordshire, Warwickshire, West Midlands and Worcestershire. The largest city in the region is Birmingham.",
+        "source": "https://en.wikipedia.org/wiki/West_Midlands_(region)"
+    },
+    "Yorkshire And The Humber": {
+        "description": "Yorkshire and the Humber is one of nine official regions of England at the first level of Nomenclature of Territorial Units for Statistics (NUTS) for statistical purposes. It comprises most of Yorkshire (the administrative areas of South Yorkshire, West Yorkshire, East Riding of Yorkshire, Hull, North Yorkshire and the City of York), as well as North Lincolnshire and North East Lincolnshire. Middlesbrough, Redcar and Cleveland or other areas of the historic county of Yorkshire, are not included. The largest settlements are Leeds, Sheffield, Bradford, Hull, and York. The population in 2011 was 5,284,000",
+        "source": "https://en.wikipedia.org/wiki/Yorkshire_and_the_Humber"
+    },
+}
