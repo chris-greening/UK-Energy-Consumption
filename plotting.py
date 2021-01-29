26823a6a0d6c4f2bac47f9074c5f85792fc655ef
chris-greening <chrisgreening3@gmail.com>
2021-01-28 22:12:24 -0500

    add: added new plot

--- plotting.py
+++ plotting.py
@@ -1,3 +1,5 @@
+import plotly
+
 ENERGY_SOURCE_COLORS = {
     "Coal": '#525B76',
     "Manufactured": '#F4A259',
@@ -7,6 +9,14 @@ ENERGY_SOURCE_COLORS = {
     "Bioenergy": '#9DBF9E',
 }
 PLOT_COLORS = {
+    'plot_bgcolor': '#fffffa',
+    'paper_bgcolor': '#fffffa'
+}
+
+CHOROPLETH_COLORS = {
     'plot_bgcolor': '#F2F8FF',
     'paper_bgcolor': '#F2F8FF'
 }
+
+places = ["United Kingdom", "Scotland", "Wales", "Northern Ireland", "East Midlands", "East Of England", "London", "North East", "North West", "South East", "South West", "West Midlands", "Yorkshire And The Humber"]
+REGION_COLORS = dict(zip(places, plotly.colors.qualitative.Prism))
