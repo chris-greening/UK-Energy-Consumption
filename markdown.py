39d9fc8441719950a06b7928c0f95213bb0451d9
chris-greening <chrisgreening3@gmail.com>
2021-01-29 11:47:47 -0500

    add: added markdown module for organizing all hardcoded MD messages

---
+++ markdown.py
@@ -0,0 +1,13 @@
+INTRO = """
+Welcome to the interactive UK Energy Consumption dashboard! The purpose of this dashboard is to visualize and explore the UK's total final energy consumption from 2005 to 2018.
+
+As you scroll down the dashboard, the scope will narrow and you'll be able to explore the United Kingdom's energy data in increasingly specific regions.
+
+Sources:
+
+- [Total final energy consumption at regional and local authority level: 2005 to 2018](https://www.gov.uk/government/statistics/total-final-energy-consumption-at-regional-and-local-authority-level-2005-to-2018)
+- [UK NUTS Level 1 (2018) boundary data](https://geoportal.statistics.gov.uk/datasets/nuts-level-1-january-2018-super-generalised-clipped-boundaries-in-the-united-kingdom)
+- [GitHub](https://github.com/chris-greening/UK-Energy-Consumption)
+
+_Notice of Non-Affiliation and Disclaimer_: We are not affiliated, associated, authorized, endorsed by, or in any way officially connected with the UK government or any of its agencies/departments. The original datasets used by this dashboard are publicly available on the UK's [government website](https://www.gov.uk/) as provided by the [Department for Business, Energy, & Industrial Strategy](https://www.gov.uk/government/organisations/department-for-business-energy-and-industrial-strategy).
+"""
